683e0b7f7322ea19b34f6445781bea364a29417d
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-03-15 15:52:23 +0100

    Add project files.

---
+++ Assiigment1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebbAppFirstCore.Models;
+
+namespace Assiigment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sledge()
+        {
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Fever()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Fcheck(int temperatur)
+        {
+            ViewBag.DoctorSays = Temperature.FeverChecker(temperatur);
+            return View();
+        }
+    }
+}
---
+++ Assiigment1/Models/Temperature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebbAppFirstCore.Models
+{
+    public class Temperature
+    {
+        
+        public static string FeverChecker(int temp, string choise)
+        //ToDo fever?
+   
+        {
+            string answer = "";
+            if (temp >= 38)
+                answer =  "Doctor said you have fever";
+            else if ( temp<= 35)
+            {
+                answer = "Docotor said you have to low temp";
+            }
+            else
+            {
+                answer = "Doctor said you have have normal";
+            }
+
+            return answer;
+        }
+        
+    }
+
+}
